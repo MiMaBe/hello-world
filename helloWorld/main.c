75f8e8a4ea58dc80f22234ee6914fd5150fa3284
mimabe <lugardelocos@gmail.com>
2016-09-09 23:23:54 +0200

    Initial Commit

---
+++ helloWorld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  helloWorld
+//
+//  Created by Miguel Martínez Bencosme on 09/09/16.
+//  Copyright © 2016 MiMaBe. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
